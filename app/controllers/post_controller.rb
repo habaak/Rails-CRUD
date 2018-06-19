4ada2b19f945cbafe2fa11f02384a6dfe729c73d
habaak <joohn728@gmail.com>
2018-06-19 07:41:25 +0000

    ADD 조건문

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -11,6 +11,7 @@ class PostController < ApplicationController
     post=Post.create(title: params[:title],
       body: params[:body]
     )
+    flash[:notice] = "글이 작성되었습니다."
     # # 1.1
     # Post.create(:title => params[:title], :body=>params[:body])
     # # 2.
@@ -31,6 +32,7 @@ class PostController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
+    flash[:alert] = "글이 삭제 되었습니다."
     redirect_to '/'
   end
 
@@ -41,6 +43,7 @@ class PostController < ApplicationController
   def update
     post=Post.find(params[:id])
     post.update(title: params[:title], body: params[:body]) # return 값이 true / false
+    flash[:notice] = "글이 수정되었습니다."
     redirect_to "/post/#{post.id}"
   end
 end
